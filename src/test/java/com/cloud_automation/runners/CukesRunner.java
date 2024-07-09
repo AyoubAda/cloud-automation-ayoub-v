4fed036135a1736050ac9a045bd08abd8b509227
aesse <a.esseakh@gmail.com>
2024-07-09 13:59:46 -0400

    Initial commit

---
+++ src/test/java/com/cloud_automation/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.cloud_automation.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/cloud_automation/step_definitions",
+        dryRun = true,
+        tags = "@wip",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
+
+
