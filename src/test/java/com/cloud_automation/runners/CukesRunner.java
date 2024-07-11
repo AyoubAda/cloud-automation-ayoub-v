64b14ea74d6c69aa6d8c24213be77fd7a496bd63
olgashyyan.uk@gmail.com <olgashyyan.uk@gmail.com>
2024-07-11 14:57:13 -0400

    Deck functionality

--- src/test/java/com/cloud_automation/runners/CukesRunner.java
+++ src/test/java/com/cloud_automation/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cloud_automation/step_definitions",
         dryRun = false,
-        tags = "@login",
+        tags = "@B34G9-115",
         publish = true
 )
 public class CukesRunner {}
